9cbb973b45a3e7cc63f799034dee6067ce6e9b48
Bruno Teixeira <bruno_teixeira@hotmail.com>
2020-06-29 17:33:01 -0300

    feito pagina home

--- routes.js
+++ routes.js
@@ -2,7 +2,9 @@ const express = require("express");
 const router = express.Router();
 
 const homeController = require("./controllers/homeController");
+const sobreController = require("./controllers/sobreController");
 
 router.get("/", homeController.home);
+router.get("/sobre", sobreController.sobre);
 
 module.exports = router;
